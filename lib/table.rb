b6a7c0fc8260d6834610b2e0cb793b094701b39f
siva.kandaraj <siva.kandaraj@myob.com>
2014-07-10 14:16:58 +1000

    Table spec are completed

--- lib/table.rb
+++ lib/table.rb
@@ -1,15 +1,18 @@
 
 class Table
   attr_accessor :grid
+  attr_reader :rows, :cols
 
+  # init the table with rows and cols
   def initialize(rows,cols)
-    @grid = Array.new(rows) { |i| Array.new(cols) { |i| 0 }}
+  	@rows = rows
+  	@cols = cols
+    @grid = Array.new(@rows) { |i| Array.new(@cols) { |i| 0 }}
   end
 
+  # check if the coordinates can fit into the table
   def is_valid?(position)
-  	valid = false
-    valid = true if  @grid[position[:rows]] && @grid[position[:rows]][position[:cols]]
-    valid
+	(position[:rows] < 0 || position[:cols] < 0 || position[:rows] >= @rows || position[:cols] >= @cols) ? false : true
   end
  
 end
--- spec/unit/table_spec.rb
+++ spec/unit/table_spec.rb
@@ -4,11 +4,7 @@ describe Table do
   
   let(:table) { Table.new(5,5) }
 
-  before :each do
-  	
-  end	 
-
-  xit('should be true if the ordinates') do
+  it('should be true if the ordinates') do
   	expect(table.is_valid?({:rows => 1, :cols => 1})).to be true
     expect(table.is_valid?({:rows => 4, :cols => 4})).to be true
     expect(table.is_valid?({:rows => 0, :cols => 4})).to be true
@@ -16,9 +12,9 @@ describe Table do
   end
 
   it('should be false if the ordinates are upper bound') do
-    #expect(table.is_valid?({:rows => 5, :cols => 1})).to be false
-    #expect(table.is_valid?({:rows => 0, :cols => 5})).to be false
-    #expect(table.is_valid?({:rows => 10, :cols => 14})).to be false
+    expect(table.is_valid?({:rows => 5, :cols => 1})).to be false
+    expect(table.is_valid?({:rows => 0, :cols => 5})).to be false
+    expect(table.is_valid?({:rows => 10, :cols => 14})).to be false
     expect(table.is_valid?({:rows => -1, :cols => -1})).to be false
   end
 
